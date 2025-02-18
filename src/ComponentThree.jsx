eb446a28a570481a8930f387d64cf519f7ce7d05
Gustavo Coutiño <gcoutino04@gmail.com>
2025-02-17 20:46:52 -0600

    Changed js to json

--- src/ComponentThree.jsx
+++ src/ComponentThree.jsx
@@ -1,6 +1,7 @@
-import { heroes } from "./data/heroes";
+import heroesData from "./data/heroes.json";
 
 export function ComponentThree() {
+  const { heroes } = heroesData;
   return (
     <div>
       <ul>
